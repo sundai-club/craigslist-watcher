933ca04b18977fb847fa12e6e469d09660f649ae
sergeicu <serge.vasylechko@tch.harvard.edu>
2024-06-02 17:42:06 -0400

    added link validation in scrape() func

--- main_micro_server/scraper.py
+++ main_micro_server/scraper.py
@@ -65,6 +65,14 @@ def translate_html_elements(timestamp: str, browser):
 def scrape(urls, hour_limit, minute_limit):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     results = {}
+    
+    # validate urls first  
+    for url in urls:
+        try:
+            if not validate_url(url):
+                raise ValueError(f"wrong link - {url}")
+        except ValueError as e:
+            print(e)        
 
     for url in urls:
         browser = browser_setup()
